19719768c096e89e27f57028e92ac8de17ab0229
jassonn95 <jasonhnguyen95@gmail.com>
2023-03-30 14:47:12 +1100

    197. Improved motion warping. Instead of warping to the target, it'll warp in the direction of the target but further away so the enemy isn't right up in your face causing issues with attack animations missing etc. Fixed an issue where player was able to hit self.

--- Public/Enemy/Enemy.h
+++ Public/Enemy/Enemy.h
@@ -38,9 +38,6 @@ protected:
 	UPROPERTY(BlueprintReadOnly)
 	EEnemyState EnemyState = EEnemyState::EES_Patrolling;
 
-	UPROPERTY(BlueprintReadOnly, Category = Combat)
-	AActor* CombatTarget;
-
 private:
 	/* AI Behaviour */
 	void InitializeEnemy();
