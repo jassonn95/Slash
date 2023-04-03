14d6f3bcf7d82b704af92dbd95938225b13a1b16
jassonn95 <jasonhnguyen95@gmail.com>
2023-04-03 12:05:55 +1000

    213. Added variables for Gold and Souls for character in AttributeComponent. Implemented getter and addition functions for those variables. Implemented variable in Soul class so we can set Soul values for different enemies for example. Implemented AddGold function for SlashCharacter. Implemented functions to add to the Soul and Gold count + update it in the UI.

--- Private/Characters/SlashCharacter.cpp
+++ Private/Characters/SlashCharacter.cpp
@@ -14,6 +14,8 @@
 #include "Animation/AnimMontage.h"
 #include "HUD/SlashHUD.h"
 #include "HUD/SlashOverlay.h"
+#include "Items/Soul.h"
+#include "Items/Treasure.h"
 
 ASlashCharacter::ASlashCharacter()
 {
@@ -122,7 +124,20 @@ void ASlashCharacter::SetOverlappingItem(AItem* Item)
 
 void ASlashCharacter::AddSouls(ASoul* Soul)
 {
-	UE_LOG(LogTemp, Warning, TEXT("ASlashCharacter::AddSouls"));
+	if (Attributes && SlashOverlay)
+	{
+		Attributes->AddSouls(Soul->GetSouls());
+		SlashOverlay->SetSouls(Attributes->GetSouls());
+	}
+}
+
+void ASlashCharacter::AddGold(ATreasure* Treasure)
+{
+	if (Attributes && SlashOverlay)
+	{
+		Attributes->AddGold(Treasure->GetGold());
+		SlashOverlay->SetGold(Attributes->GetGold());
+	}
 }
 
 void ASlashCharacter::EKeyPressed()
--- Public/Characters/SlashCharacter.h
+++ Public/Characters/SlashCharacter.h
@@ -15,6 +15,7 @@ class USpringArmComponent;
 class UCameraComponent;
 class AItem;
 class ASoul;
+class ATreasure;
 class UAnimMontage;
 class USlashOverlay;
 
@@ -31,6 +32,7 @@ public:
 	virtual void GetHit_Implementation(const FVector& ImpactPoint, AActor* Hitter) override;
 	virtual void SetOverlappingItem(AItem* Item) override;
 	virtual void AddSouls(ASoul* Soul) override;
+	virtual void AddGold(ATreasure* Treasure) override;
 
 protected:
 	virtual void BeginPlay() override;
